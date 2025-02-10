890669c2d204075b41e55af70199edf5fa924c61
Yeakub <yeakub@nexstack.sg>
2025-02-10 12:44:15 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
-hello data
+hello new data
            <div>
       {Array.from({ length: 10 }, (_, index) => (
         <p key={index}>{userName}</p>
