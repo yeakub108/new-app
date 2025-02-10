0aafe6c4e0b1bd84ea7df3155523b55c8ac3ec9e
Yeakub <yeakub@nexstack.sg>
2025-02-10 13:14:55 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <h1>User Name:</h1>
+        <h1>User Names:</h1>
            <div>
       {Array.from({ length: 10 }, (_, index) => (
         <p key={index}>{userName}</p>
