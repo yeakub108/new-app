79f2aa6a02c6b922777ceeefdbd93bca870e9710
Yeakub <yeakub@nexstack.sg>
2025-02-11 14:31:56 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
 
-const datas = "Data"
-const data = "Hello Datass"
-  console.log(datas)
+const Names =["Apple", "orange", "foots"];
+  console.log(Names)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
