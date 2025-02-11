71991a4c2055b6a87ad69d357b8d26c889ca26c8
Yeakub <yeakub@nexstack.sg>
2025-02-11 12:45:49 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
 
 const datas = "Data"
-const data = "Datass"
+const data = "Hello Datass"
   console.log(datas)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
