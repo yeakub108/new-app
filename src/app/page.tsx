d37032144d057c86c4391a09b8a13de67c99c2a6
Yeakub <yeakub@nexstack.sg>
2025-02-12 12:04:48 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
     egfdf
     dfgff
   fhsoae
+  add data
     
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
