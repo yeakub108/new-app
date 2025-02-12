060ac77f815392da51adc757417dbf2a8902200c
Yeakub <yeakub@nexstack.sg>
2025-02-12 16:31:13 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+
 
 export default function Home() {
 
 
-  consts fruitNames ====
+  const fruitNames =[
     "Apple",
     "Banana",
     "Cherry",
@@ -15,6 +15,7 @@ export default function Home() {
     "Strawberry",
     "Watermelons",
     "Data"
+    ]
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
