e5ec505b9377d8c05a96043e2611e324a3507d11
Yeakub <yeakub@nexstack.sg>
2025-02-13 10:06:50 +0600

    Update page.tsx (#50)
    * Update page.tsx
    
    * Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,11 +23,12 @@ export default function Home() {
           src="/next.svg"
           alt="Next.js logo"
           width={180}
-          height={38}
+          height={38}dd
           priority
         />
        <div>
       <h1>Fruit Names</h1>
+         fruitNames.map(index)=>[
       <ul>
         {fruitNames.map((fruit, index) => (
           <li key={index}>{fruit}</li>
