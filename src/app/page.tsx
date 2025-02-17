f6ff4e273d0fe56e19d46d1e5125ef44e4c0a0c5
Yeakub <yeakub@nexstack.sg>
2025-02-18 01:10:58 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,7 @@ const {name, setNames} = useState()
 const userName = "Yeakub"
 
   const user= "mother"
-  const fruitNames = [
-  const user ="hello"
+  const user ="hello
   const fruitNames =[
     "Mango",
     "Apple",
@@ -19,7 +18,7 @@ const userName = "Yeakub"
     "Peach",
     "Pineapple",
     "Strawberry",
-    "Watermelons",
+    "Watermelons"
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
