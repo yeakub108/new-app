086394fe1cfc6af69e083d11883c4b518f6b6972
Yeakub <yeakub@nexstack.sg>
2025-02-17 12:25:22 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 const {name, setNames}= useState()
 const userName = Yeakub
   console.log(userName)
-  cnst user ="hello"
+  const user ="hello"
   const fruitNames =[
     "Apple",
     "Banana",
