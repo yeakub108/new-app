74286a0952c717325fcefd394c431ac53599f950
Yeakub <yeakub@nexstack.sg>
2025-02-18 12:22:50 +0600

    Update page.tsx (#73)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ export default function Home() {
 const {name, setNames} = useState()
 const userName = "Yeakub
 
-  const user= "mother"
-  const fruitNames = [
   const user ="hello"
   const fruitNames =[
     "Mango",
@@ -20,7 +18,7 @@ const userName = "Yeakub
     "Pineapple",
     "Strawberry",
     "Watermelons",
-
+]
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
