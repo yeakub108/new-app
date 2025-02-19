a7dfc9295c55c22dccce81e40bd8bfb10173d06b
Yeakub <yeakub@nexstack.sg>
2025-02-19 11:46:40 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const userName = [
     "Peach",
     "Pineapple",
     "Strawberry",
-    "Watermelons",
+    "Watermelons"
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
