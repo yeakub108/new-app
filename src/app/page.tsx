a671e9d9b461b84d9850daabebd1772aec09ba66
Yeakub <yeakub@nexstack.sg>
2025-02-20 11:16:23 +0600

    Update page.tsx (#88)
    * Update page.tsx
    
    * Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import {useState} from "react";
 export default function Home() {
 const {name, setNames} = useState()
 
-const userName = [
+const fruitNames = [
     "Mango",
     "Apple",
     "Banana",
@@ -15,7 +15,7 @@ const userName = [
     "Peach",
     "Pineapple",
     "Strawberry",
-    "Watermelons",
+]
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
