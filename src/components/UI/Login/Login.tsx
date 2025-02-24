9d55818507cd110826c6b02bf859012723989c4b
Yeakub <yeakub@nexstack.sg>
2025-02-24 18:30:53 +0600

    Update Login.tsx

--- src/components/UI/Login/Login.tsx
+++ src/components/UI/Login/Login.tsx
@@ -1,10 +1,20 @@
-"use client"
+"use client";
+import { useEffect } from "react";
 
 const Login = () => {
+    useEffect(() => {
+        console.log("Login component mounted");
+
+        return () => {
+            console.log("Login component unmounted");
+        };
+    }, []);
+
     return (
         <div>
             <h1>This is Login</h1>
         </div>
     );
 };
+
 export default Login;
