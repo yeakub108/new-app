23c4f07e22c773d2126fb598c43f8335b635da15
Yeakub <yeakub@nexstack.sg>
2025-02-24 15:52:36 +0600

    Update SignIn.tsx (#105)
    * Update SignIn.tsx
    
    * Update SignIn.tsx
    
    * Update SignIn.tsx
    
    * Update SignIn.tsx

--- src/components/UI/Login/SignIn.tsx
+++ src/components/UI/Login/SignIn.tsx
@@ -1,1 +1,13 @@
 
+"use client"
+
+const Login = () => {
+    const userNames = "Yeakub"
+    
+    return (
+        <div>
+            <h1>This is Logins</h1>
+        </div>
+    );
+};
+export default Login;
