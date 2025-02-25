698438aa03f9d5ef64f817df6d917e5c351ad3b4
Yeakub <yeakub@nexstack.sg>
2025-02-25 10:19:24 +0600

    Update page.tsx (#109)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import Image from "next/image";
 import {useState} from "react";
 
@@ -18,7 +16,7 @@ const fruitNames = [
     "Peach",
     "Pineapple",
     "Strawberry",
-
+]
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
