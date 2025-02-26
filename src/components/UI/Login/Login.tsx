ec8c71cfae5f5b948db7d532149b2b94aae1532f
Yeakub <yeakub@nexstack.sg>
2025-02-26 10:37:05 +0600

    Update Login.tsx (#108)
    * Update Login.tsx
    
    * Update Login.tsx
    
    * Update Login.tsx
    
    * Update Login.tsx

--- src/components/UI/Login/Login.tsx
+++ src/components/UI/Login/Login.tsx
@@ -1,9 +1,16 @@
+"use client";
+import { useState } from "react";
+
 
 const Login = () => {
+  const useState [user, setUser] = useState()
+
     return (
         <div>
             <h1>This is Login</h1>
+          <p>Hello</p>
         </div>
     );
 };
+
 export default Login;
