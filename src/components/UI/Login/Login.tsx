b3344834b9bd11f9011d689cf9ffb79262776d76
Yeakub <yeakub@nexstack.sg>
2025-03-03 16:02:01 +0600

    Update Login.tsx (#113)

--- src/components/UI/Login/Login.tsx
+++ src/components/UI/Login/Login.tsx
@@ -8,8 +8,11 @@ const Login = () => {
     return (
         <div>
             <h1>This is Login</h1>
+
+            <p className="text-3xl font-bold bg-red-500">Login</p>
             <p>Hello</p>
           <p>Hello</p>
+
         </div>
     );
 };
